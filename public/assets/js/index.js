6f9540a0db0df9adf9feb56d6570432aa10302d6
Michael Souter <souterm1@gmail.com>
2021-07-17 21:32:07 -0400

    CSS and JS files now rendering after correctly applying express.static method

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -24,7 +24,7 @@ const hide = (elem) => {
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
-
+//MAY NEED TO MODIFY//
 const getNotes = () =>
   fetch("/api/notes", {
     method: "GET",
@@ -32,7 +32,7 @@ const getNotes = () =>
       "Content-Type": "application/json",
     },
   });
-
+//MAY NEED TO MODIFY//
 const saveNote = (note) =>
   fetch("/api/notes", {
     method: "POST",
@@ -41,7 +41,7 @@ const saveNote = (note) =>
     },
     body: JSON.stringify(note),
   });
-
+//MAY NEED TO MODIFY//
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
     method: "DELETE",
--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.get("/index", (req, res) =>
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
-
-app.use(express.static(__dirname + "/public/assets"));
 
 app.get("/api/notes", (req, res) => res.json(noteList));
 
